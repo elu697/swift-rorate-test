44d35ca4e8d3ad125589359b78e7b826783d6a8b
tomoki hirayama <tomoki_hirayama@cyberagent.co.jp>
2021-03-09 13:51:04 +0900

    Initial Commit

---
+++ iOS-Rotate-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Rotate-Test
+//
+//  Created by 平山 智己 on 2021/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
